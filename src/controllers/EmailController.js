7eba5bc11b4fbf9da228ad8d3dd4736fc27dc58c
ClemersonDBA <clemerson20@hotmail.com>
2020-12-21 23:08:08 -0300

    up

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -10,14 +10,11 @@ module.exports = {
             service: "gmail",
             host: "smtp.gmail.com",
             port: 587,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
-            tls: {
-                rejectUnauthorized: false,
-            }
         }));
 
         transporter.sendMail({
